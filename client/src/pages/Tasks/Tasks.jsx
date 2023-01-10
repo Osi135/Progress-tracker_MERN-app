6aa885a2f5273ea55f3b1a082dce6bf1c95e7200
Osi135 <oscar.horn.roostami@outlook.com>
2023-01-10 15:23:34 +0100

    Client has GET and POST requests integrated

--- client/src/pages/Tasks/Tasks.jsx
+++ client/src/pages/Tasks/Tasks.jsx
@@ -1,13 +1,70 @@
-import React from 'react'
-import './Tasks.scss'
+import React, { useState, useEffect } from 'react';
+import './Tasks.scss';
 import { images } from '../../constants';
 import { NavbarWrap } from '../../wrapper';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState({
+    title: ''
+  });
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8082/api/tasks')
+      .then((res) => {
+        setTasks(res.data);
+      })
+      .catch((err) => {
+        console.log('Error from ShowBookList');
+      });
+  }, [])
+  
+
+  const taskList = tasks.length === 0
+    ? 'There is no tasks'
+    : tasks.map((task) => <h2>{task.title}</h2>);
+  
+  const onChange = (e) => {
+    setTask({ ...task, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post('http://localhost:8082/api/tasks', task)
+      .then((res) => {
+        setTask({
+          title: ''
+        });
+      })
+      .catch((err) => {
+        console.log('Error in CreateTask!');
+      });
+  };
+
   return (
     <div>
       <p>Tasks</p>
       <img src={images.react} alt="react logo" />
+      <div>{taskList}</div>
+      <form  noValidate onSubmit={onSubmit}>
+        <input
+          type='text'
+          placeholder='Title of the Task'
+          name='title'
+          value={task.title}
+          onChange={onChange}
+        />
+        <input
+          type='submit'
+        />
+      </form>
     </div>
     
 
